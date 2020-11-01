47a56a4c9b821f46c0b55f9a51abfe4826e16c5e
Roberto-Livi <robertomlivi@outlook.com>
2020-11-01 17:24:42 -0500

    created MobilePhone class, and transferred the functionality from DeskPhone to MobilePhone

---
+++ code/src/com/company/MobilePhone.java
@@ -0,0 +1,49 @@
+package com.company;
+
+public class MobilePhone implements ITelephone {
+
+    private int myNumber;
+    private boolean isRinging;
+    private boolean isOn = false;
+
+    public MobilePhone(int myNumber) {
+        this.myNumber = myNumber;
+    }
+
+    @Override
+    public void powerOn() {
+        isOn = true;
+        System.out.println("Mobile phone powered up");
+    }
+
+    @Override
+    public void dial(int phoneNumber) {
+        System.out.println("Now ringing " + phoneNumber + " on deskphone");
+    }
+
+    @Override
+    public void answer() {
+        if (isRinging) {
+            System.out.println("Answering the desk phone");
+            isRinging = false;
+        }
+    }
+
+    @Override
+    public boolean callPhone(int phoneNumber) {
+        if (phoneNumber == myNumber) {
+            isRinging = true;
+            System.out.println("Ring ring");
+        } else {
+            isRinging = false;
+        }
+
+        return isRinging;
+    }
+
+    @Override
+    public boolean isRinging() {
+        return isRinging;
+    }
+
+}
